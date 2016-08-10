4efceaf143969221ad0c91b6010fac15f39f2aa2
ullik <ulrike.koeppen@br.de>
2016-08-10 14:54:57 +0200

    modify graphics, add sources

--- analysis_norm.R
+++ analysis_norm.R
@@ -8,6 +8,10 @@ athl <- read.csv("LA-teilnehmer.csv", quote="", encoding="utf-8", sep=";")
 #fälle pro jahr
 cpy <- cases %>% group_by(Jahr, Geschlecht) %>% summarize(count=length(unique(Name))) %>%
   group_by(Jahr) %>% summarize(count=sum(count))# %>% arrange(-count)
+tmp <- athl %>% group_by(Jahr, Geschlecht) %>% summarize(count=length(unique(Name))) %>%
+  group_by(Jahr) %>% summarize(sum = sum(count))
+cpy <- left_join(cpy, tmp, by=c("Jahr")) %>% mutate(norm = count/sum)
+
 write.csv(cpy, "fälleprojahr.csv", row.names = F)
 
 #fälle pro land
@@ -45,6 +49,10 @@ cpgy <- left_join(cpgy, tmp, by=c("Geschlecht","Jahr")) %>% mutate(norm = count/
 write.csv(cpg, "fällenachgeschlecht.csv", row.names = F)
 write.csv(cpgy, "fällenachgeschlechtundjahr.csv", row.names = F)
 
+#tests pro jahr
+tests <- read.csv("testnrs.csv")[c(1,3)]
+tests$Number.of.tests <- as.numeric(gsub(",","",tests$Number.of.tests))
+
 save.image(file="datasets.Rdata")
 
 ##################
@@ -58,6 +66,17 @@ pdf("results/alle-normiert.pdf", width=14)
 ggplot(cpy, aes(x=Jahr, y=count)) + theme_light() + scale_x_continuous(breaks = rev(unique(cpgy$Jahr))) +
   ggtitle("Doping-Fälle über die Jahre") + geom_line(colour="orange") + geom_point() +
   geom_text(aes(x=Jahr, label=paste0(count)), vjust=-1)
+'
+#fälle pro jahr normiert
+ggplot(cpy, aes(x=Jahr, y=norm)) + theme_light() + scale_x_continuous(breaks = rev(unique(cpgy$Jahr))) +
+  ggtitle("Anteil überführter Athleten über die Jahre") + geom_line(colour="orange") + geom_point() +
+  geom_text(aes(x=Jahr, label=paste0(round(norm*100, 2),"%")), vjust=-1) + scale_y_continuous(labels=scales::percent)
+'
+
+#tests pro jahr
+ggplot(tests, aes(x=Year, y=Number.of.tests)) + theme_light() + scale_x_continuous(breaks = rev(unique(tests$Year))) +
+  ggtitle("Doping-Tests über die Jahre") + geom_line(colour="orange") + geom_point() +
+  geom_text(aes(x=Year, label=paste0(Number.of.tests)), vjust=-1)
 
 #fälle pro land
 #sonstiges kategorie einrichten
@@ -65,23 +84,28 @@ cpc <- cpc %>% ungroup %>% arrange(-count)
 x <- cpc %>% mutate(Land = c(cpc$Land[1:5], rep("Sonstige",length(cpc$Land)-5)))
 x <- mutate(x, Land = factor(x$Land, levels=c("Türkei", "Russland","Belarus","Ukraine","USA","Sonstige"))) %>%
   group_by(Land) %>% summarize(count=sum(count), sum=sum(sum), norm=count/sum)
+#sonstiges summe und norm
+tmp <- athl %>% group_by(Land, Jahr, Geschlecht) %>% summarize(count=length(unique(Name))) %>%
+  group_by(Land) %>% summarize(sum = sum(count))
+tmp2 = filter(tmp, Land != "TUR" & Land != "RUS" & Land != "BLR" & Land != "UKR" & Land != "USA")
+x$sum[6] <- sum(tmp2$sum); x$norm[6] <- x$count[6] / x$sum[6]
 #gesamtdurchschnitt
-cmean = sum(cpc$count)/sum(cpc$sum) #0.01231637
-ggplot(x, aes(x=Land, y=norm, fill=Land)) + theme_minimal() +
+cmean = sum(cpc$count)/sum(tmp$sum) #0.00514601
+ggplot(x, aes(x=Land, y=norm, fill=Land)) + theme_minimal()  + scale_y_continuous(labels=scales::percent) +
   geom_bar(stat="identity") + ggtitle("Doping-Fälle nach Ländern") +
   geom_hline(yintercept = cmean) + theme(legend.position="none") +
-  geom_text(aes(x=1, y=cmean, label=paste("Gesamtdurchschnitt =", round(cmean,4))), hjust=0,vjust=-0.5)+
+  geom_text(aes(x=1, y=cmean, label=paste("Gesamtdurchschnitt =", paste0(round(cmean*100,2),"%"))), hjust=0,vjust=-0.5)+
   geom_text(position= position_dodge(width=0.9), aes(x=Land, label=paste0(count,"/",sum)), vjust=-0.5)
 
 #fälle nach geschlecht
 cpg <- cpg %>% ungroup %>% arrange(-norm)
-ggplot(cpg, aes(x=Geschlecht, y=norm, fill=Geschlecht)) + theme_minimal() +
+ggplot(cpg, aes(x=Geschlecht, y=norm, fill=Geschlecht)) + theme_minimal() + scale_y_continuous(labels=scales::percent) +
   geom_bar(stat="identity") + ggtitle("Doping-Fälle nach Geschlecht") +
   theme(legend.position="none", axis.text.x = element_text(angle=45,hjust=1), panel.background = element_rect(fill="white")) +
   geom_text(position= position_dodge(width=0.9), aes(x=Geschlecht, label=paste0(count,"/",sum)), vjust=-0.5)
 
 #fälle nach geschlecht und jahr
-cpgy <- cpgy %>% ungroup %>% arrange(-norm)
+'cpgy <- cpgy %>% ungroup %>% arrange(-norm)
 ggplot(cpgy, aes(x=Jahr, y=norm, fill=Geschlecht)) + scale_x_continuous(breaks = rev(unique(cpgy$Jahr))) +theme_minimal() +
   geom_bar(stat="identity",position="dodge") + ggtitle("Doping-Fälle nach Geschlecht und Jahr") +
   theme(legend.position="bottom", panel.background = element_rect(fill="white")) +
@@ -102,7 +126,7 @@ ggplot(cpk, aes(x=Kategorie, y=norm, fill=Kategorie)) +
   geom_bar(stat="identity") + ggtitle("Doping-Fälle nach Kategorie") +
   theme(legend.position="none", axis.text.x = element_text(angle=45,hjust=1), panel.background = element_rect(fill="white")) +
   geom_text(position= position_dodge(width=0.9), aes(x=Kategorie, label=paste0(count,"/",sum)), vjust=-0.5)
-
+'
 dev.off()
 
 #zahlenabgleich
@@ -125,3 +149,8 @@ wnrs <- athl %>% group_by(Jahr) %>% filter(Geschlecht == "Women") %>%
 dnrs <- athl %>% group_by(Jahr, Geschlecht) %>% summarize(count = length(unique(Disziplin))) %>%
   group_by(Jahr) %>% summarize(count = sum(count)) %>% left_join(iaaf[c(1,7)], by=c("Jahr"="Year")) %>%
   mutate(diff = count-Events, perc=abs(1-(count/Events)))
+
+
+#32
+
+athl %>% group_by(Jahr) %>% filter(Land == "TUR") %>% summarize(n = length(unique(Name)))
