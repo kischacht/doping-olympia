f8e9c562508f9f0af2e8621181c1dfb676db35be
ullik <ulrike.koeppen@br.de>
2016-08-17 11:54:22 +0200

    update results graphics

--- analysis_norm.R
+++ analysis_norm.R
@@ -12,7 +12,7 @@ athl$Jahr <- as.numeric(substr(athl$Geschlecht,2,5))
 athl$Geschlecht <- gsub("^X.*\\.","",athl$Geschlecht)
 names(athl)[1] = "Land"
 write.csv(athl, "iaaf-teilnehmer.csv", row.names = F)
-
+athl <- read.csv("iaaf-teilnehmer.csv",stringsAsFactors = F)
 #fälle pro jahr
 cpy <- cases %>% group_by(Jahr) %>% summarize(count=length(unique(Name)))# %>%
   group_by(Jahr) %>% summarize(count=sum(count))# %>% arrange(-count)
@@ -55,10 +55,10 @@ save.image(file="datasets.Rdata")
 ##################
 
 library(ggplot2)
-pdf("results/doping-results.pdf", width=14)
+pdf("results/alle-normiert.pdf", width=14)
 
 #fälle pro jahr
-ggplot(cpy, aes(x=Jahr, y=count)) + theme_light() + scale_x_continuous(breaks = rev(unique(cpgy$Jahr))) +
+ggplot(cpy, aes(x=Jahr, y=count)) + theme_light() + scale_x_continuous(breaks = rev(unique(cpy$Jahr))) +
   ggtitle("Doping Violations in Athletics at Olympic Games") + geom_line(colour="orange") + geom_point() +
   geom_text(aes(x=Jahr, label=paste0(count)), vjust=-1)
 '
@@ -66,20 +66,20 @@ ggplot(cpy, aes(x=Jahr, y=count)) + theme_light() + scale_x_continuous(breaks = 
 ggplot(cpy, aes(x=Jahr, y=norm)) + theme_light() + scale_x_continuous(breaks = rev(unique(cpgy$Jahr))) +
   ggtitle("Anteil überführter Athleten über die Jahre") + geom_line(colour="orange") + geom_point() +
   geom_text(aes(x=Jahr, label=paste0(round(norm*100, 2),"%")), vjust=-1) + scale_y_continuous(labels=scales::percent)
-'
+
 
 #tests pro jahr
 ggplot(tests, aes(x=Year, y=Number.of.tests)) + theme_light() + scale_x_continuous(breaks = rev(unique(tests$Year))) +
   ggtitle("Gesamtzahl an Doping-Tests bei den olympischen Spielen") + geom_line(colour="orange") + geom_point() +
   geom_text(aes(x=Year, label=paste0(Number.of.tests)), vjust=-1)
-
+'
 #fälle pro land
 #sonstiges kategorie einrichten
 cpc <- cpc %>% ungroup %>% arrange(-count)
 x <- cpc %>% mutate(Land = c(cpc$Land[1:5], rep("Sonstige",length(cpc$Land)-5)))
 x <- mutate(x, Land = factor(x$Land, levels=c("Türkei","Belarus","Ukraine","Russland","USA","Sonstige"))) %>%
   group_by(Land) %>% summarize(count=sum(count), sum=sum(sum), norm=count/sum)
-x$Land <- factor(c("Turkey","Belarus","Ukraine","Russia","USA","Others"), levels=c("Turkey","Belarus","Ukraine","Russia","USA","Others"))
+#x$Land <- factor(c("Turkey","Belarus","Ukraine","Russia","USA","Others"), levels=c("Turkey","Belarus","Ukraine","Russia","USA","Others"))
 #sonstiges summe und norm
 tmp <- athl %>% filter(Jahr >= 1996) %>% group_by(Land) %>% summarize(sum = sum(count))
 tmp2 = filter(tmp, Land != "TUR" & Land != "RUS" & Land != "BLR" & Land != "UKR" & Land != "USA")
@@ -87,14 +87,14 @@ x$sum[6] <- sum(tmp2$sum); x$norm[6] <- x$count[6] / x$sum[6]
 #gesamtdurchschnitt
 cmean = sum(cpc$count)/sum(tmp$sum) #0.00514601
 ggplot(x, aes(x=Land, y=norm, fill=Land)) + theme_minimal()  + scale_y_continuous(labels=scales::percent) +
-  geom_bar(stat="identity") + ggtitle("Share of Athletes with Annulled olympic performances due to Doping since incl. 1996") +
+  geom_bar(stat="identity") + ggtitle("Anteil überführter Leichtathleten seit 1996") +
   geom_hline(yintercept = cmean) + theme(legend.position="none") +
   geom_text(aes(x=1, y=cmean, label=paste("Total mean =", paste0(round(cmean*100,2),"%"))), hjust=0,vjust=-0.5)+
   geom_text(position= position_dodge(width=0.9), aes(x=Land, label=paste0(count,"/",sum)), vjust=-0.5)
 
 #fälle nach geschlecht
 cpg <- cpg %>% ungroup
-ggplot(cpg, aes(x=Geschlecht, y=count, fill=Geschlecht)) + theme_minimal() + scale_y_continuous(labels=scales::percent) +
+ggplot(cpg, aes(x=Geschlecht, y=count, fill=Geschlecht)) + theme_minimal() + #scale_y_continuous(labels=scales::percent) +
   geom_bar(stat="identity") + ggtitle("Anzahl überführter Leichtathleten nach Geschlecht seit 1996") +
   theme(legend.position="none", axis.text.x = element_text(angle=45,hjust=1), panel.background = element_rect(fill="white")) +
   geom_text(position= position_dodge(width=0.9), aes(x=Geschlecht, label=paste0(count,"/",sum)), vjust=-0.5)
